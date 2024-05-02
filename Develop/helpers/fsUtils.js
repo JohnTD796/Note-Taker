4f355f1b684dbe6511c2b30506726089f67682bf
JohnTD796 <156860528+JohnTD796@users.noreply.github.com>
2024-05-02 16:39:51 -0400

    Update fsUtils.js
    removed initial notes from the original file and made my own

--- Develop/helpers/fsUtils.js
+++ Develop/helpers/fsUtils.js
@@ -1,24 +1,16 @@
 const fs = require('fs');
 const util = require('util');
 
-// Promise version of fs.readFile
+//function to read from a file.
 const readFromFile = util.promisify(fs.readFile);
-/**
- *  Function to write data to the JSON file given a destination and some content
- *  @param {string} destination The file you want to write to.
- *  @param {object} content The content you want to write to the file.
- *  @returns {void} Nothing
- */
+
+//function used to write information to a file.
 const writeToFile = (destination, content) =>
   fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
     err ? console.error(err) : console.info(`\nData written to ${destination}`)
   );
-/**
- *  Function to read data from a given a file and append some content
- *  @param {object} content The content you want to append to the file.
- *  @param {string} file The path to the file you want to save to.
- *  @returns {void} Nothing
- */
+
+//function used to update a pre-existing file with new information.
 const readAndAppend = (content, file) => {
   fs.readFile(file, 'utf8', (err, data) => {
     if (err) {
